a4191165796ce3354436ad9db0bc3f65e871b6b5
rim-code <friouirim@gmail.com>
2019-12-03 10:40:34 +0100

    Updates

---
+++ precache-manifest.b5438b6a74d30b1a2666e3ade059250a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b45ee93b77755d3391bedc2505be35",
+    "url": "/docplannerreact/index.html"
+  },
+  {
+    "revision": "cd2252ac805e3bb94ab3",
+    "url": "/docplannerreact/static/css/main.28f2d308.chunk.css"
+  },
+  {
+    "revision": "f8a43e35d6f8cf784c5b",
+    "url": "/docplannerreact/static/js/2.61f5f17e.chunk.js"
+  },
+  {
+    "revision": "cd2252ac805e3bb94ab3",
+    "url": "/docplannerreact/static/js/main.0b22c2bf.chunk.js"
+  },
+  {
+    "revision": "eccb1eac065f72bf7eab",
+    "url": "/docplannerreact/static/js/runtime-main.6e14ca37.js"
+  }
+]);
